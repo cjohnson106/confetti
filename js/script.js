81bf511857450ac0927e5c59022b21e3b2d548df
cjohnson106 <cjohnson106@ewu.edu>
2022-10-13 10:59:11 -0700

    Added confetti
    *Added confetti js
    *Added html
    *Added css
    *Added js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // My Scripts
 
 console.log('Page Loaded');
+
+// Run it
+let confetti = new Confetti("confetti");
+
+// Optionalize it
+confetti.setCount(1000);
+confetti.setSize(1);
+confetti.setPower(50);
+confetti.setFade(false);
+confetti.destroyTarget(false);
